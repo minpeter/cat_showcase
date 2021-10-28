6b73289e280eef8cc3641c4e64581e3ed42ad38f
minpeter <kali2005611@gmail.com>
2021-10-29 08:47:23 +0900

    loaded img successful

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
-function App() {
-  return <h1>happe hacking!!</h1>;
+import React from "react";
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    cats: [],
+  };
+
+  getImageUrl = async () => {
+    const { data } = await axios.get(
+      "https://api.thecatapi.com/v1/images/search"
+    );
+    this.setState({ cats: data, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getImageUrl();
+  }
+
+  render() {
+    const { isLoading, cats } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="cats">
+            <h3>Loaded</h3>
+            <div>
+              {cats.map((cat) => (
+                <img key={cat.id} src={cat.url} alt="cat" />
+              ))}
+            </div>
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
